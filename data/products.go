0762f1681f158f90aa106b59c4073e77d652e6be
Dijo Mathews <dijo.mathews@gmail.com>
2022-07-14 22:37:45 +0530

    c3

---
+++ data/products.go
@@ -0,0 +1,35 @@
+package data
+
+import "time"
+
+type Product struct {
+	ID          int
+	Name        string
+	Description string
+	Price       float32
+	SKU         string
+	CreatedOn   string
+	UpdatedOn   string
+	DeletedOn   string
+}
+
+var productList = []*Product{
+	&Product{
+		ID:          1,
+		Name:        "latte",
+		Description: "frothy milky coffee",
+		Price:       2.33,
+		SKU:         "333sss",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+	&Product{
+		ID:          1,
+		Name:        "latte",
+		Description: "frothy milky coffee",
+		Price:       2.33,
+		SKU:         "333sss",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+}
