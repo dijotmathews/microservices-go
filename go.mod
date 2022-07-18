07ecfb23f9f2ae6860ccba62e36c8444a345a5c0
Dijo Mathews <dijo.mathews@gmail.com>
2022-07-18 09:00:06 +0530

    router refactor

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/microservices-go/vendor
 
 go 1.18
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- vendor/handlers/products.go
+++ vendor/handlers/products.go
@@ -4,57 +4,69 @@ import (
 	"data"
 	"log"
 	"net/http"
-	"regexp"
 	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
+// Products is ...
 type Products struct {
 	l *log.Logger
 }
 
+// NewProducts is
 func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
-func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		p.getProducts(rw, r)
-		return
+// func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+// 	if r.Method == http.MethodGet {
+// 		p.getProducts(rw, r)
+// 		return
+// 	}
+
+// 	if r.Method == http.MethodPost {
+// 		p.addProduct(rw, r)
+// 		return
+// 	}
+
+// 	if r.Method == http.MethodPut {
+// 		reg := regexp.MustCompile(`/([0-9]+)`)
+// 		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
+
+// 		if len(g) != 1 {
+// 			http.Error(rw, "Invalid URL", http.StatusBadRequest)
+// 			return
+// 		}
+// 		if len(g[0]) != 2 {
+// 			http.Error(rw, "Invalid URL", http.StatusBadRequest)
+// 			return
+// 		}
+
+// 		idString := g[0][1]
+// 		id, err := strconv.Atoi(idString)
+
+// 		if err != nil {
+// 			http.Error(rw, "Invalid URL", http.StatusBadRequest)
+// 			return
+
+// 		}
+// 		p.l.Println("got id ", id)
+// 		p.updateProducts(id, rw, r)
+// 		return
+// 	}
+// }
+
+// UpdateProducts ...
+func (p *Products) UpdateProducts(rw http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	id, perr := strconv.Atoi(vars["id"])
+	if perr != nil {
+		http.Error(rw, "Invalid URL", http.StatusBadRequest)
+
 	}
 
-	if r.Method == http.MethodPost {
-		p.addProduct(rw, r)
-		return
-	}
-
-	if r.Method == http.MethodPut {
-		reg := regexp.MustCompile(`/([0-9]+)`)
-		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
-
-		if len(g) != 1 {
-			http.Error(rw, "Invalid URL", http.StatusBadRequest)
-			return
-		}
-		if len(g[0]) != 2 {
-			http.Error(rw, "Invalid URL", http.StatusBadRequest)
-			return
-		}
-
-		idString := g[0][1]
-		id, err := strconv.Atoi(idString)
-
-		if err != nil {
-			http.Error(rw, "Invalid URL", http.StatusBadRequest)
-			return
-
-		}
-		p.l.Println("got id ", id)
-		p.updateProducts(id, rw, r)
-		return
-	}
-}
-
-func (p *Products) updateProducts(id int, rw http.ResponseWriter, r *http.Request) {
 	p.l.Println("handle PUT product")
 	prod := &data.Product{}
 	err := prod.FromJSON(r.Body)
@@ -75,7 +87,8 @@ func (p *Products) updateProducts(id int, rw http.ResponseWriter, r *http.Reques
 
 }
 
-func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+// AddProduct ...
+func (p *Products) AddProduct(rw http.ResponseWriter, r *http.Request) {
 	p.l.Println("handle POST product")
 	prod := &data.Product{}
 	err := prod.FromJSON(r.Body)
@@ -85,7 +98,8 @@ func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
 	data.AddProduct(prod)
 }
 
-func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+// GetProducts ...
+func (p *Products) GetProducts(rw http.ResponseWriter, r *http.Request) {
 	lp := data.GetProducts()
 	err := lp.ToJSON(rw)
 	if err != nil {
